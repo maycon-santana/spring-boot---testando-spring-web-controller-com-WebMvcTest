19e8d5dc1f542cc03215891ce1dfcb2e9e8099bd
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-11-11 11:26:35 -0300

    Criando o projeto

---
+++ src/test/java/br/com/mayconinforgames/demostracao/webmvctest/WebmvctestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.demostracao.webmvctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmvctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
